a8a1f73c69159126f0513a0fd78bafdd1f6843e6
susonthapa <33973551+susonthapa@users.noreply.github.com>
2025-03-23 14:31:05 +0545

    added nested text

--- src/Components/LikeImageCard.tsx
+++ src/Components/LikeImageCard.tsx
@@ -10,6 +10,7 @@ export default function LikeImageCard(props: {picture: Picture}) {
     <view className="picture-wrapper">
       <image style={{width: "100%", aspectRatio: picture.width / picture.height}} src={picture.src} />
       <LikeIcon />
+      <text style={{fontSize: 16, fontWeight: "bold", color: "white"}}>Hello WOrld <text style={{color: "red"}}>Nested text</text></text>
     </view>
   )
 }
